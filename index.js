c5bba08f76320b475dee28ca117410cd4ebc171e
Greg Azzaretti <azzaretti.greg@gmail.com>
2019-03-26 00:09:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const dodger = document.getElementById('dodger')
 
 function onKeydown(event) {
   if(event.keyCode === 37){
-     console.log(event.keyCode)
+     console.log(event.style.left)
   }
 }
 
