3b0e9cad53bb873a81c1ece51bcf7ecd82b29c9a
Greg Azzaretti <azzaretti.greg@gmail.com>
2019-03-26 00:28:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -30,18 +30,17 @@ function onKeydown(event) {
   if(event.keyCode === 39){
     moveDodgerRightDodgerRight()
   }
-  if(event.keyCode === 40)
-  move
-}
-  
+  if(event.keyCode === 40){
+  moveDodgerDown()
+  }
   if(event.keyCode == 38){
-    const currentPosition = parseInt(dodger.style.bottom)
-    dodger.style.bottom = currentPosition + 5 + 'px'
+    moveDodgerUp()
   }
-  if(event.keyCode == 40){
-    const currentPosition = parseInt(dodger.style.bottom)
-    dodger.style.bottom = currentPosition - 5 + 'px'
-  }
+}
+  // if(event.keyCode == 40){
+  //   const currentPosition = parseInt(dodger.style.bottom)
+  //   dodger.style.bottom = currentPosition - 5 + 'px'
+  // }
 
 
 document.addEventListener('keydown', onKeydown)
