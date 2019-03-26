42a82e6025b57c194df57ea67bc4e3cff619bcb9
Greg Azzaretti <azzaretti.greg@gmail.com>
2019-03-26 00:25:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,25 +7,29 @@ function noscroll() {
 window.addEventListener('scroll', noscroll);
 
 
-
+function makeDodgerRight(){
+  const currentPosition = parseInt(dodger.style.left)
+  dodger.style.left = currentPosition + 5 + 'px'
+}
 
 function onKeydown(event) {
   if(event.keyCode === 37){
     const currentPosition = parseInt(dodger.style.left)
      dodger.style.left = currentPosition - 5 + 'px'
   }
-  if(event.keyCode === 39){
-    const currentPosition = parseInt(dodger.style.left)
-     dodger.style.left = currentPosition + 5 + 'px'
-  }
-  if(event.keyCode == 38){
-    const currentPosition = parseInt(dodger.style.bottom)
-     dodger.style.bottom = currentPosition + 5 + 'px'
-  }
-  if(event.keyCode == 40){
-    const currentPosition = parseInt(dodger.style.bottom)
-     dodger.style.bottom = currentPosition - 5 + 'px'
+  else if(event.keyCode === 39){
+    makeDodgerRight()
   }
 }
+  
+  // if(event.keyCode == 38){
+  //   const currentPosition = parseInt(dodger.style.bottom)
+  //   dodger.style.bottom = currentPosition + 5 + 'px'
+  // }
+  // if(event.keyCode == 40){
+  //   const currentPosition = parseInt(dodger.style.bottom)
+  //   dodger.style.bottom = currentPosition - 5 + 'px'
+  // }
+
 
 document.addEventListener('keydown', onKeydown)
